0ea03b248180a05cab9a87b6622821eb84be638a
Jordan <jstrocketry@gmail.com>
2018-07-05 22:14:22 -0400

    needs an arduino program to work with. also needs to be tested

--- main.py
+++ main.py
@@ -12,9 +12,94 @@ style.use("ggplot")
 from matplotlib.widgets import Slider
 
 
+orientationData = []
 root = Tk()
 
+class SyntaxParser():
+    prevTime = 0
+    prevSyntax = None
+    START_LINE = '@{'
+    END_LINE = '}@'
+    TIMING = ';'
+    GYRO_SYNTAX = ["GX:", "GY:", "GZ:", "TS:"]
+    ACCEL_SYNTAX = ["AX:", "AY:", "AZ:", "TS:"]
+    ORIENTATION_SYNTAX = ["OX:", "OY:", "OZ:", "TS:"]
+    PRESSURE_SYNTAX = ["PS:", "TS:"]
+    ALTITUDE_SYNTAX = ["PA:", "TS:"]
+
+    def parseLine(self, line):
+        print(line)
+        toAppend = 0.0
+        syntax = self.getSyntax(line)
+        data = []
+        startIndex = line.index(self.START_LINE)
+        endIndex = line.index(self.END_LINE)
+        if self.goodLine(line):
+            for i in range(0, len(syntax)):
+                if(i < len(syntax)-1):
+                    toAppend = float(line[line.index(syntax[i])+3:line.index(syntax[i+1])-1])
+                else:
+                    toAppend = float(line[line.index(syntax[i])+3:endIndex])
+                    if toAppend <= self.prevTime and syntax == self.prevSyntax:
+                        return None
+                    self.prevTime = toAppend
+                data.append(toAppend)
+                self.prevSyntax = syntax
+            return data
+        else: return None
+
+    def getSyntax(self, line):
+        startIndex = line.index(self.START_LINE)
+        testSyntax = line[startIndex+2:startIndex+5]
+        if(testSyntax == self.GYRO_SYNTAX[0]):
+            return self.GYRO_SYNTAX
+        elif(testSyntax == self.ACCEL_SYNTAX[0]):
+            return self.ACCEL_SYNTAX
+        elif(testSyntax == self.ORIENTATION_SYNTAX[0]):
+            return self.ORIENTATION_SYNTAX
+        elif(testSyntax == self.PRESSURE_SYNTAX[0]):
+            return self.PRESSURE_SYNTAX
+        elif(testSyntax == self.ALTITUDE_SYNTAX[0]):
+            return self.ALTITUDE_SYNTAX
+
+    def goodLine(self, line):
+        startIndex = line.index(self.START_LINE)
+        endIndex = line.index(self.END_LINE)
+        syntax = self.getSyntax(line)
+        timingCount = self.getTimingCount(line)
+        if startIndex is not None and endIndex is not None and syntax is not None:
+            if timingCount is len(syntax)-1:
+                return True
+        return False
+
+    def getTimingCount(self, line):
+        count = 0;
+        for i in line:
+            if i is self.TIMING:
+                count += 1
+        return count
+
+class ArduinoCommunicator():
+    def __init__(self, port):
+        self.ser = serial.Serial(port, 115200)
+
+    def readData(self):
+        return str(self.ser.readline())
+
+    def isAvailable(self):
+        return self.ser.is_open
+
+    def writeData(self, data):
+        self.ser.write(data + "\n")
+
+    def kill(self):
+        self.ser.close()
+
 class GUI():
+    ox = []
+    oy = []
+    oz = []
+    ts = []
     def __init__(self, master):
         master.columnconfigure(0,weight=1)
         master.rowconfigure(1,weight=1)
@@ -56,63 +141,69 @@ class GUI():
         # self.canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
         self.toolbar = NavigationToolbar2TkAgg(self.canvas, self.frame)
         self.slider = Slider(self.sliderAxis, "X Orientation", -90, 90, valinit=0, valstep=1)
-        self.slider.on_changed(self.updateData0)
+        self.slider.on_changed(self.plotGraph)
         self.slider1 = Slider(self.sliderAxis1, "Y Orientation", -90, 90, valinit=0, valstep=1)
-        self.slider1.on_changed(self.updateData1)
+        self.slider1.on_changed(self.plotGraph)
         self.slider2 = Slider(self.sliderAxis2, "Z Orientation", -90, 90, valinit=0, valstep=1)
-        self.slider2.on_changed(self.updateData2)
+        self.slider2.on_changed(self.plotGraph)
         # self.toolbar.grid(column = 0, row = 2, columnspan=2)
         self.toolbar.update()
 
     def plotGraph(self):
         xData = []
         yData = []
-        for i in range(0,5):
-            xData.append(i)
-            yData.append(self.slider.val)
-        self.line, = self.ax0.plot(xData,yData)
+        zData = []
+        for i in range(0,len(self.ts)):
+            xData.append(self.slider.val)
+            yData.append(self.slider1.val)
+            zData.append(self.slider2.val)
+        comms.writeData(self.slider.val)
+        comms.writeData(self.slider1.val)
+        comms.writeData(self.slider2.val)
+        self.line, = self.ax0.plot(self.ts,xData)
         self.line.set_color("blue")
-        self.line1, = self.ax1.plot(xData,yData)
+        self.line1, = self.ax1.plot(self.ts,yData)
         self.line1.set_color("blue")
-        self.line2, = self.ax2.plot(xData,yData)
+        self.line2, = self.ax2.plot(self.ts,zData)
         self.line2.set_color("blue")
+        self.addOrientationData()
+        self.l = self.ax0.plot(self.ts, self.ox)
+        self.l.set_color("black")
+        self.l1 = self.ax1.plot(self.ts, self.oy)
+        self.l1.set_color("black")
+        self.l2 = self.ax2.plot(self.ts, self.oy)
+        self.l2.set_color("black")
         self.canvas.draw()
 
-    def updateData0(self, val):
-        xData = []
-        yData = []
-        self.line.set_linestyle('')
-        for i in range(0,5):
-            xData.append(i)
-            yData.append(val)
-        self.line, = self.ax0.plot(xData,yData)
-        self.line.set_color("blue")
-        self.canvas.draw()
+    def addOrientationData(self):
+        self.ox = []
+        self.oy = []
+        self.oz = []
+        self.ts = []
+        for i in orientationData:
+            self.ox.append(i[0])
+            self.oy.append(i[1])
+            self.oz.append(i[2])
+            self.ts.append(i[3])
 
-    def updateData1(self, val):
-        xData = []
-        yData = []
-        self.line1.set_linestyle('')
-        for i in range(0,5):
-            xData.append(i)
-            yData.append(val)
-        self.line1, = self.ax1.plot(xData,yData)
-        self.line1.set_color("blue")
-        self.canvas.draw()
-
-    def updateData2(self, val):
-        xData = []
-        yData = []
-        self.line2.set_linestyle('')
-        for i in range(0,5):
-            xData.append(i)
-            yData.append(val)
-        self.line2, = self.ax2.plot(xData,yData)
-        self.line2.set_color("blue")
-        self.canvas.draw()
+def runArduino():
+    print("start thread")
+    parser = SyntaxParser()
+    f = comms.readData()
+    while comms.isAvailable():
+        if parser.START_LINE in f and parser.END_LINE in f:
+            sampleData = parser.parseLine(f)
+            if(sampleData != None):
+                orientationData.append(sampleData)
+                graph.plotGraph()
+            f = comms.readData()
+        else:
+            f = comms.readData()
 
 def main():
-    graph = GUI(root)
+    _thread.start_new_thread(runArduino, ())
     root.mainloop()
 
+comms = ArduinoCommunicator("/dev/ttyACM0")
+graph = GUI(root)
 main()
